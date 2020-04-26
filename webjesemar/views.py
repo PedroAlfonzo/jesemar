67efdc87e12b31c956bef262768b421cc82b6ba9
PedroAlfonzo <pedro21086@gmail.com>
2020-04-26 15:40:00 -0400

    arreglar direcciones de doc statics

--- webjesemar/views.py
+++ webjesemar/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def webjesemar_list(request):
-    return render(request, 'webjesemar/webjesemar_list.html', {})
+    return render(request, 'webjesemar_list.html', {})
